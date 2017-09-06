fbadc0f0350c59cc1e08f1a9bd76b8f7a1e4beaf
voltrue2 <voltrue2@gmail.com>
2017-09-06 17:01:12 +0900

    detched cheerio in favor for JSDOM

--- test.js
+++ test.js
@@ -1,18 +1,24 @@
 'use strict';
 
+const Jsdom = require('jsdom').JSDOM;
 const req = require('request');
 const iconv = require('iconv-lite');
 
 const params = {
 	encoding: null, // we want the body to be a binary
-	url: 'http://nora-ebisu.com/'
+	url: 'http://www.ekiten.jp'
 };
 
 req(params, function (error, res, body) {
 	if (error) {
 		return console.error(error);
 	}
-	var data = iconv.decode(body, 'Shift_JIS');
-	//var data = iconv.decode(body, 'EUC-JP');
-	console.log(data);
+	var data = iconv.decode(body, 'EUC-JP');
+	//console.log(data);
+	var dom = new Jsdom(data);
+	var doc = dom.window.document;
+	var list = doc.querySelectorAll('.l-footer-ekiten_link a');
+	for (var i = 0, len = list.length; i < len; i++) {
+		console.log(i, list[i].textContent);
+	}
 });
